4dd1da390ccc7115d1c87c4b2117943a6d4282e9
Shaun Sundquist <sund@mac.com>
2015-06-05 16:00:13 -0400

    a good start

--- rsync.netBackup.sh
+++ rsync.netBackup.sh
@@ -18,8 +18,10 @@
 ### in cron job, the path may be just /bin and /usr/bin
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:$PATH"
 #
-PDIR=$(dirname $(readlink -f $0))
-confFile="$PDIR/auto-gitlab-backup.conf"
+OSNAME=`uname -s` #
+PDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+confFile="$PDIR/rsync.netBackup.conf"
+
 
 ###
 ## Functions
@@ -27,10 +29,12 @@ confFile="$PDIR/auto-gitlab-backup.conf"
 
 checkSize() {
     echo ===== Sizing =====
-    echo "Total disk space used for backup storage.."
-    echo "Size - Location"
-    echo `du -hs "$gitRakeBackups"`
     echo
+}
+
+rsyncConnection() {
+  echo "Testing connection to $rsyncUSER@$rsyncSERVER..."
+  ssh $rsyncSERVER -l $rsyncUSER -T ls > /dev/null
 }
 
 rsyncKey() {
@@ -55,7 +59,24 @@ sshQuotaKey() {
 printScriptver() {
 	# print the most recent tag
 	echo "This is $0"
-	echo "Version $(git describe --abbrev=0 --tags), commit #$(git log --pretty=format:'%h' -n 1)."
+  version=$(git describe --abbrev=0 --tags 2>/dev/null)
+  if [ $? -eq 0 ]
+    then
+    echo "Version $version, commit #$(git log --pretty=format:'%h' -n 1)."
+  fi
+}
+
+determineOS() {
+case "$OSNAME" in
+    "Linux")
+        ;;
+    "Darwin")
+        ;;
+    "SunOS")
+        ;;
+    *)
+        ;;
+esac
 }
 
 ###
@@ -67,13 +88,36 @@ if [ -e $confFile -a -r $confFile ]
 then
 	source $confFile
 	echo "Parsing config file..."
-        rvm_ENV
 else
-	echo "No confFile found; Remote copy DISABLED."
+	echo "No config file found; Please create a config file."
+  echo "See https://github.com/sund/rsync.netBackup/wiki/Configuration"
+  exit 1
 fi
 
-# go back to where we came from
-cd $PDIR
+if [[ -z "$rsyncSSHKEY" ]] # if var is NOT empty
+then
+  echo "using default key"
+else
+  echo -n "config supplied valid ssh key "
+  echo $rsyncSSHKEY
+fi
+
+if [ -z "$rsyncUSER" ]; then echo "rsyncUSER is unset"; else echo "rsyncUSER is set to '$rsyncUSER'"; fi
+if [ -z "$rsyncSERVER" ]; then echo "rsyncSERVER is unset"; else echo "rsyncSERVER is set to '$rsyncSERVER'"; fi
+if [ -z "$rsyncSSHKEY" ]; then echo "rsyncSSHKEY is unset"; else echo "rsyncSSHKEY is set to '$rsyncSSHKEY'"; fi
+
+# check account creds
+if [ -z "$rsyncUSER" ] && [ -z "$rsyncSERVER" ] && [ -e $rsyncSSHKEY -a -r $rsyncSSHKEY ]
+  then
+  rsyncConnection
+else
+  echo "Error connecting to $rsyncUSER@$rsyncSERVER."
+  echo "See https://github.com/sund/rsync.netBackup/wiki/Configuration"
+  exit 1
+fi
+
+# what type/kind of host are we on
+determineOS
 
 # Print version
 printScriptver
